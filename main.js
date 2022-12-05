2838d1702396e8ed52d45441e9b4deae2383b9a0
EdwinVladimirP <edwinmontV87@hotmail.com>
2022-12-05 15:20:43 +0100

    actualizado 05/12/2022 15:20

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+
+console.log('hola')
 const header = document.querySelector("header"); 
 
-window.addEventListener("scroll", function(){
+window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 0); 
 }); 
+
+let menu = document.querySelector('#menu-icon'); 
+let navlist = document.querySelector('.navlist'); 
+
+
+/*menu.addEventListener('click', () => alert('you pressed'))*/
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-xxxxxx'); 
+    navlist.classList.toggle('active');
+    
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x'); 
+    navlist.classList.remove('active');
+}
+
+
